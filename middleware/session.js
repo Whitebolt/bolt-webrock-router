eec3f01e5e63a326e48fc8dc4a7cf8f2600d44e7
Kristjan Allikas <Kristjan Allikas>
2016-05-06 15:32:43 +0100

    Add mongo-connect as the session store
    Updated session middleware to include mongo-connect reusing the existing
    connection

--- middleware/session.js
+++ middleware/session.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 module.exports = function(app) {
   app.use(session({
-    secret: app.config.secret
+    secret: app.config.secret,
+    store: new MongoStore({
+      db: app.dbs.main
+    })
   }));
 };
