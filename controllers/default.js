006c7fb8585eb56c9b55a3703f66375b056dc0c3
Kristjan Allikas <Kristjan Allikas>
2016-05-06 12:48:57 +0100

    Adds multiple database instances
    Config for multiple databases
    Page load from database

--- controllers/default.js
+++ controllers/default.js
@@ -2,8 +2,24 @@
 
 module.exports = {
   getPage: function(req, res, next) {
-    console.log("GET PAGE!");
-    res.send('Hello World on port ');
-    next();
+    console.log('GET PAGE!');
+
+    let title, content;
+
+    req.app.dbs['main'].collection('page').findOne({'path':req.path}, function(err, doc) {
+        if(doc) {
+          console.log(doc);
+          title = doc.title;
+          content = doc.content;
+
+          console.log(title);
+          console.log(content);
+
+          res.send('<h1>' + title + '</h1><p>' + content + '</p>');
+          res.end();
+        } else {
+          next();
+        }
+    });
   }
 };
--- index.js
+++ index.js
@@ -2,18 +2,57 @@
 
 const express = require('express');
 const app = express();
+const Promise = require('bluebird');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 app.config = require('./server.json');
 
-middlewareLoader(app);
-controllerLoader(app);
 
+
+loadDatabases(app).then(() => {
+  middlewareLoader(app);
+  controllerLoader(app);
+
+  app.listen(app.config.port, () => {
+    console.log('Express Listening on port ' + app.config.port);
+  });
+});
+
+/*mongoConnect(app.config.mongoDbUrl).then(database => {
+  console.log("MongoDB Connected to server: " + app.config.mongoDbUrl);
+  app.db = database;
+
+  return database;
+}).then(database => {
+  
+});*/
+
+//MongoClient.connect(app.config.mongoDbUrl, function(err, database) {
+  //assert.equal(null, err);
+ 
+//});
+  
 /*app.get('/', (req, res) => {
   res.send('Hello World on port ' + app.config.port);
 });*/
 
-app.listen(app.config.port, () => {
-  console.log('Express Listening on port ' + app.config.port);
-});
+function loadDatabases(app) {
+  const mongoConnect = Promise.promisify(MongoClient.connect);
+  app.dbs = app.dbs || {};
+
+  let dbPromises = [];
+
+  Object.keys(app.config.databases).forEach(dbName => {
+    let conn = app.config.databases[dbName];
+
+    dbPromises.push(mongoConnect(conn).then(database => {
+      app.dbs[dbName] = database;
+      return database;
+    }));
+  });
+
+  return Promise.all(dbPromises);
+}
 
 function controllerLoader(app) {
   app.controllers = app.controllers || {},
@@ -22,6 +61,7 @@ function controllerLoader(app) {
   });
 
   app.all(/^\/api\/.*/, (req, res, next) => {
+    req.app = app;
     let path = req.path.replace(/^\/api\//, '').split('/');
     if (path.length) {
       let controller = path[0];
@@ -43,6 +83,7 @@ function controllerLoader(app) {
   });
 
   app.all(/\/.*/, (req, res, next) => {
+    req.app = app;
     let controller = app.config.controllers.default.controller;
     let method = app.config.controllers.default.method;
 
