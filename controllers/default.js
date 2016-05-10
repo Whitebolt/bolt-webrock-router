e15c12572584d8ff035a4676a74ce1eef281cf7f
Kristjan Allikas <Kristjan Allikas>
2016-05-10 13:39:59 +0100

    Menu Structuralisation

--- controllers/default.js
+++ controllers/default.js
@@ -1,23 +1,69 @@
 'use strict';
 
-module.exports = {
-  getPage: function(req, res, next) {
-    console.log('GET PAGE!');
+const Promise = require('bluebird');
 
-    req.app.dbs['main'].collection('page').findOne({
-      'path': req.path.replace(/\/$/, '')
-    }, function(err, doc) {
-        if(doc) {
-          let template = (doc.template ?
-            req.app.templates[doc.template] :
-            req.app.templates['default']
-          );
-         
-          res.send(template(doc));
-          res.end();
-        } else {
-          next();
-        }
+function getPath(req) {
+  let path = req.path.trim().replace(/\/$/, '');
+
+  return ((path === '') ? '/' : path);
+}
+
+function setActive(doc, items) {
+  let itemSet = false;
+  (items || []).forEach(item => {
+    if (item.path === doc.path) {
+      item.active = true;
+      itemSet = true;
+    } else {
+      item.active = false;
+    }
+
+    if (setActive(doc, item.items)) {
+      item.active = true;
+    }
+  });
+  return itemSet;
+}
+
+function getMenu(menuName, db, mainDoc) {
+  return db.collection('menus').findOne({"name": menuName}).then(doc => {
+    if (doc) {
+      mainDoc.menu = doc;
+      setActive(mainDoc, doc.items)
+    }
+    return mainDoc;
+  }, err => {
+    return mainDoc;
+  });
+}
+
+let exported = {
+  getPage: function(req, res, next) {
+    console.log('GET ' + req.path);
+
+    req.app.db.collection('pages').findOne({
+      'path': getPath(req)
+    }).then(doc => {
+      if (!doc) {
+        throw "Document not found in Database";
+      }
+
+      req.template = (doc.template ?
+        req.app.templates[doc.template] :
+        req.app.templates['default']
+      );
+
+      return getMenu("main", req.app.db, doc);
+
+    }).then(doc => {
+      console.log(doc);
+      res.send(req.template(doc));
+      res.end();
+    }, err => {
+      console.log(err);
+      next();
     });
   }
 };
+
+module.exports = exported;
