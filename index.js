fb705f1ed5d2a78862336a3029b10ee0698b081b
Stephen <stephen@whitebolt.net>
2016-05-24 20:55:24 +0100

    Do some more async loading
    Apply greater consistancy

--- index.js
+++ index.js
@@ -6,37 +6,36 @@ console.log('\n'+'[' + ' server init '.green + '] ' +Date().toLocaleString());
 const Promise = require('bluebird');
 const readFile = Promise.promisify(require('fs').readFile);
 
-require('require-extra')('./lib/bolt/').then(bolt => {
-  global.bolt = bolt;
-  global.express = require('express');
+require('require-extra')([
+	'./lib/bolt/', 'express', './lib/loaders', process.argv[2]
+]).spread((bolt, express, loaders, config) => {
+	Object.assign(global, {bolt, express});
 
-  return bolt.require(['./lib/loaders', process.argv[2]]);
-}).spread((loaders, config) => {
-  const app = express();
-  app.config = config;
-  app.config.template = app.config.template || 'index';
+	const app = express();
+	app.config = config;
+	app.config.template = app.config.template || 'index';
 
-  /**
-   * @todo These should all load at once instead of in sequence.
-   */
-  loaders.databases.load(app).then(() => {
-    app.middleware = app.middleware || {};
-    return loaders.middleware.load(app, app.config.root, app.middleware);
-  }).then(() => {
-    return loaders.routes.load(app);
-  }).then(() => {
-    return loaders.components.load(app, loaders, app.config.root);
-  }).then(() => {
-    app.templates = app.templates || {};
-    return loaders.templates.load(app.config.root, app.templates);
-  }).then(() => {
-    app.listen(app.config.port, () => {
-      console.log('[' + ' listen '.green + '] ' + 'Bolt Server on port ' + app.config.port.toString().green + '\n\n');
-      readFile('./welcome.txt', 'utf-8').then(welcome => {
-        console.log(welcome);
-        console.log('\n'+'[' + ' load complete '.green + '] ' +Date().toLocaleString());
+	/**
+	 * @todo These should all load at once instead of in sequence.
+	 */
+	loaders.databases.load(app).then(() => {
+		app.middleware = app.middleware || {};
+		return loaders.middleware.load(app, app.config.root, app.middleware);
+	}).then(() => {
+		return loaders.routes.load(app);
+	}).then(() => {
+		return loaders.components.load(app, loaders, app.config.root);
+	}).then(() => {
+		app.templates = app.templates || {};
+		return loaders.templates.load(app.config.root, app.templates);
+	}).then(() => {
+		app.listen(app.config.port, () => {
+			console.log('[' + ' listen '.green + '] ' + 'Bolt Server on port ' + app.config.port.toString().green + '\n\n');
+			readFile('./welcome.txt', 'utf-8').then(welcome => {
+				console.log(welcome);
+				console.log('\n'+'[' + ' load complete '.green + '] ' +Date().toLocaleString());
 
-      });      
-    });
-  });
+			});
+		});
+	});
 });
