e59a02b52d590657645f7aada0243bd98e685c33
Stephen <stephen@whitebolt.net>
2016-05-25 10:41:49 +0100

    Add config loader
    Load package.json and its config into config as well as supplied config.  Package config allows default values and new loader gives options for more advance spec, such as use of environment variables.
    
    Uses package variables to construct banner.
    
    Set to v3.0.0 as this is what it is

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 'use strict';
 
-const colour = require('colors');
 const Promise = require('bluebird');
 const ascii = require('ascii-art');
-const readFile = Promise.promisify(require('fs').readFile);
 ascii.Figlet.fontPath = './console_fonts/';
 
-console.log('\n'+'[' + colour.green(' server init ') + '] ' +Date().toLocaleString());
 
 
 require('require-extra')([
   'express',
   './lib/bolt/files',
   './lib/loaders/bolt',
-  process.argv[2]
-]).spread((express, boltFs, boltLoader, config) => {
+  './lib/loaders/config',
+  'colors'
+]).spread((express, boltFs, boltLoader, configLoader, colour) => {
+  console.log('\n'+'[' + colour.green(' server init ') + '] ' +Date().toLocaleString());
+
   const app = express();
   const loaders = {};
 
-  app.config = config;
-  app.config.template = app.config.template || 'index';
-
-  boltLoader.load(app.config.root).then(bolt => {
+  configLoader.load(app).then(config => {
+    return boltLoader.load(app.config.root);
+  }).then(bolt => {
     return Object.assign(global, {bolt, express});
   }).then(() => boltFs.importDirectory('./lib/loaders', loaders)).then(() => {
     return loaders.databases.load(app);
@@ -36,12 +35,10 @@ require('require-extra')([
     app.listen(app.config.port, () => {
       console.log('[' + ' listen '.green + '] ' + 'Bolt Server on port ' + app.config.port.toString().green + '\n\n');
       ascii
-        .font('Bolt', 'big', 'bright_blue+bold')
-        .font('Server', 'big', 'bright_cyan+bold')
-        .font(' v3.0', 'big', 'white+bold', banner => {
-          readFile('./LICENCE.md', 'utf-8').then(licence => {
-            console.log(banner + '\n ' + colour.white(licence) + '\n\n');
-          });
+        .font(bolt.titleCase(app.config.name.split('-').shift()), 'big', 'bright_blue+bold')
+        .font(bolt.titleCase(app.config.name.split('-').slice(1).join(' ')), 'big', 'bright_cyan+bold')
+        .font(' v' + app.config.version, 'big', 'white+bold', banner => {
+          console.log(banner + '\n ' + colour.white(app.config.license) + '\n\n');
         });
     });
   });
