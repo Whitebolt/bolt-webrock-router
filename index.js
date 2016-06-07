eafd7ae68e79cc170f73a50129ddb9ac334ce05f
Stephen <stephen@whitebolt.net>
2016-06-07 13:20:56 +0100

    Use importDirectory correctly
    The require-extra version has a different signature, follow this.  Also, use some of the new features this supplies.

--- index.js
+++ index.js
@@ -2,36 +2,36 @@
 
 const Promise = require('bluebird');
 const readFile = Promise.promisify(require('fs').readFile);
+const loaders = {};
 
+/**
+ * @todo These should all load at once instead of in sequence.
+ */
 require('./lib/').then(() => {
-  return bolt.require.importDirectory('./lib/loaders/');
+  return bolt.require.importDirectory('./lib/loaders/', {imports: loaders});
 }).then(loaders => {
-  /**
-   * @todo These should all load at once instead of in sequence.
-   */
+  return loaders.app.load(process.argv[2]);
+}).then(app => {
+  return loaders.database.load(app);
+}).then(app => {
+  return loaders.middleware.load(app, app.config.root, app.middleware);
+}).then(app => {
+  return loaders.route.load(app);
+}).then(app => {
+  return loaders.component.load(app, loaders, app.config.root);
+}).then(app => {
+  return loaders.template.loadViewContent(app.config.root, app.templates).then(() => app);
+}).then(app => {
+  return loaders.template.load(app.config.root, app.templates, app.config.template).then(() => app);
+}).then(app => {
+  return loaders.template.compileAllViews(app);
+}).then(app => {
+  app.listen(app.config.port, () => {
+    console.log('[' + ' listen '.green + '] ' + 'Bolt Server on port ' + app.config.port.toString().green + '\n\n');
+    readFile('./welcome.txt', 'utf-8').then(welcome => {
+      console.log(welcome);
+      console.log('\n'+'[' + ' load complete '.green + '] ' +Date().toLocaleString());
 
-  loaders.app.load(process.argv[2]).then(app => {
-    return loaders.database.load(app);
-  }).then(app => {
-    return loaders.middleware.load(app, app.config.root, app.middleware);
-  }).then(app => {
-    return loaders.route.load(app);
-  }).then(app => {
-    return loaders.component.load(app, loaders, app.config.root);
-  }).then(app => {
-    return loaders.template.loadViewContent(app.config.root, app.templates).then(() => app);
-  }).then(app => {
-    return loaders.template.load(app.config.root, app.templates, app.config.template).then(() => app);
-  }).then(app => {
-    return loaders.template.compileAllViews(app);
-  }).then(app => {
-    app.listen(app.config.port, () => {
-      console.log('[' + ' listen '.green + '] ' + 'Bolt Server on port ' + app.config.port.toString().green + '\n\n');
-      readFile('./welcome.txt', 'utf-8').then(welcome => {
-        console.log(welcome);
-        console.log('\n'+'[' + ' load complete '.green + '] ' +Date().toLocaleString());
-
-      });
     });
   });
 });
