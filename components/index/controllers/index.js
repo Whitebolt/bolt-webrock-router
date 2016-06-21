2e81b6b8699ec059b2eef25870990a80272179a2
Stephen <stephen@whitebolt.net>
2016-06-21 08:49:34 +0100

    Finalise componet methods

--- components/index/controllers/index.js
+++ components/index/controllers/index.js
@@ -2,35 +2,6 @@
 
 const Promise = require('bluebird');
 
-function setActive(doc, items) {
-  let itemSet = false;
-  (items || []).forEach(item => {
-    if (item.path === doc.path) {
-      item.active = true;
-      itemSet = true;
-    } else {
-      item.active = false;
-    }
-
-    if (setActive(doc, item.items)) {
-      item.active = true;
-    }
-  });
-  return itemSet;
-}
-
-function getMenu(menuName, db, mainDoc) {
-  return db.collection('menus').findOne({"name": menuName}).then(doc => {
-    if (doc) {
-      mainDoc.menu = doc;
-      setActive(mainDoc, doc.items)
-    }
-    return mainDoc;
-  }, err => {
-    return mainDoc;
-  });
-}
-
 function getPath(req) {
   let path = req.path.trim().replace(/\/$/, '');
 
@@ -49,8 +20,7 @@ let exported = {
       let template = ((doc.view && req.app.templates[doc.view]) ? doc.view : 'index');
       req.doc = doc;
 
-      return getMenu("main", req.app.db, doc)
-        .then(doc => {return {template}});
+      return {template}
     });
   }
 };
---
+++ components/menu/controllers/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function setActive(doc, items) {
+  let itemSet = false;
+  (items || []).forEach(item => {
+    if (item.path === doc.path) {
+      item.active = true;
+      itemSet = true;
+    } else {
+      item.active = false;
+    }
+
+    if (setActive(doc, item.items)) {
+      item.active = true;
+    }
+  });
+  return itemSet;
+}
+
+function getMenu(menuName, req) {
+  return req.app.db.collection('menus').findOne({"name": menuName}).then(doc => {
+    if (doc) {
+      req.doc.menu = doc;
+      setActive(req.doc, doc.items)
+    }
+
+    return {};
+  }, err => {
+    return {};
+  });
+}
+
+let exported = {
+  index: function(req) {
+    req.doc = req.doc || {};
+    return getMenu("main", req);
+  }
+};
+
+module.exports = exported;
