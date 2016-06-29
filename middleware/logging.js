9dc5b5a7dc1365190b854a1e6afc432f33d1d27a
Stephen <stephen@whitebolt.net>
2016-06-29 16:49:21 +0100

    Add access log logging

---
+++ middleware/logging.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const morgan = require('morgan');
+const Writable = require('stream').Writable;
+
+function init(app) {
+  const stream = new Writable({
+    write(chunk, encoding, callback) {
+      bolt.broadcast("access-log", chunk.toString());
+      callback();
+    }
+  });
+  app.use(morgan('combined', {stream}));
+}
+
+init.priority = 7;
+module.exports = init;
