b7f071da7f22c7d19d6fd3c2a46a0cf156e63d49
Stephen <stephen@whitebolt.net>
2016-07-15 08:44:08 +0100

    Load app via hooks
    Rewrite of loading sequence to use the new hooks interface.

--- index.js
+++ index.js
@@ -4,15 +4,39 @@ global.boltRootDir = __dirname;
 
 const loaders = {};
 
-/**
- * @todo These should all load at once instead of in sequence.
- */
-require('./lib/')
-  .then(() => bolt.require.importDirectory('./lib/loaders/', {imports: loaders}))
-  .then(loaders => loaders.app.load(process.argv[2]))
-  .then(app => loaders.database.load(app))
-  .then(app => loaders.middleware.load(app, app.config.root, app.middleware))
-  .then(app => loaders.route.load(app))
-  .then(app => loaders.component.load(app, loaders, app.config.root))
-  .then(app => loaders.template.load(app))
-  .then(app => loaders.run(app));
+function importLoaders(dirPath, importObj) {
+  return bolt.require.importDirectory(dirPath, {imports: importObj})
+    .then(()=> bolt.fire('loadersImported', importObj))
+}
+
+require('./lib/').then(bolt => {
+  bolt.hook('loadersImported', () => {
+    loaders.app.load(process.argv[2]);
+  });
+
+  bolt.hook('initialiseAppDone', (hook, app) => {
+    loaders.database.load(app);
+  });
+
+  bolt.hook('loadDatabasesDone', (hook, app) => {
+    loaders.middleware.load(app, app.config.root, app.middleware);
+  });
+
+  bolt.hook('loadMiddlewareDone', (hook, app) => {
+    loaders.route.load(app);
+  });
+
+  bolt.hook('loadRoutesDone', (hook, app) => {
+    loaders.component.load(app, loaders, app.config.root);
+  });
+
+  bolt.hook('loadAllComponentsDone', (hook, app) => {
+    loaders.template.load(app);
+  });
+
+  bolt.hook('loadTemplatesDone', (hook, app) => {
+    loaders.run(app);
+  });
+
+  importLoaders('./lib/loaders/', loaders);
+});
