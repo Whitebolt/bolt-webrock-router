6d4b06a57a12284b759bbda584a729ac6a64a61d
Stephen <stephen@whitebolt.net>
2016-08-23 12:25:30 +0100

    Move bolt to sub-directory of root and remove extra index file
    Now there is no separate loading of loaders we can simplify loading and project structure.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 'use strict';
 
 global.boltRootDir = __dirname;
+global.colour = require('colors');
+global.express = require('express');
+global.bolt = Object.assign(require('lodash'));
 
-require('./lib/').then(bolt => {
+require('require-extra').importDirectory('./bolt/', {
+  merge: true,
+  imports: bolt
+}).then(bolt => {
   bolt.hook('afterInitialiseApp', (hook, configPath, app) => bolt.loadHooks(app));
   bolt.loadApplication(process.argv[2]);
 });
