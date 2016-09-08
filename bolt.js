dc50c6f571e4b265b272dc62f64e43bac1c8ae61
Stephen <stephen@whitebolt.net>
2016-09-08 10:10:39 +0100

    Move db loaders into interface files
    Create separate files for each database loader and for the global db logic.

--- bolt.js
+++ bolt.js
@@ -2,7 +2,6 @@
 'use strict';
 
 const Promise = require('bluebird');
-const dbBolt = require('./bolt/database');
 const pm2 = require('bluebird').promisifyAll(require('pm2'));
 const _ = require('lodash');
 let linuxUser;
@@ -22,101 +21,105 @@ if (!argv.development && argv.d) argv.development = argv.d;
 if (!argv.development && !argv.d) argv.development = false;
 
 
-function parseConfig(config) {
-  config.script = __dirname + '/server.js';
-  let template = _.template(JSON.stringify(config));
-  return JSON.parse(template(config));
-}
+require('./bolt/database').then(dbBolt=>{
 
-function loadConfig(name) {
-  return dbBolt.loadMongo(config.db)
-    .then(db=>db.collection('configs').findOne({name}))
-    .then(parseConfig)
-    .then(siteConfig=>{
-      siteConfig.development = (siteConfig.hasOwnProperty('development') ? siteConfig.development : argv.development);
-      if (!linuxUser) siteConfig.development = true;
-      return siteConfig;
-    })
-}
-
-function getPm2Instances(name) {
-  return pm2.listAsync()
-    .filter(apps=>(apps.name === name));
-}
-
-function removeOldInstances(pm2Config) {
-  return getPm2Instances(pm2Config.name).then(apps=>(
-    apps.length ?
-      Promise.all(apps.map(app=>pm2.deleteAsync(app.pm2_env.pm_id))) :
-      true
-  ));
-}
-
-function startInstance(pm2Config, boltConfig) {
-  return pm2.startAsync(pm2Config).then(app=>{
-    const id = app[0].pm2_env.pm_id;
-    pm2.sendDataToProcessId(id, {type:'config', data:boltConfig, id, topic:'config'});
-    return pm2.disconnectAsync().then(()=>app[0]);
-  });
-}
-
-function launchPm2(siteConfig) {
-  let pm2Config = _.pick(siteConfig, processFileProperties);
-  let boltConfig = _.pick(siteConfig, boltConfigProperties);
-
-  return pm2.connectAsync()
-    .then(()=>removeOldInstances(pm2Config))
-    .then(()=>startInstance(pm2Config, boltConfig));
-}
-
-function launchApp(siteConfig) {
-  let boltConfig = _.pick(siteConfig, boltConfigProperties);
-  launcher(boltConfig);
-}
-
-function createUserIfNotCreated(isUser, siteConfig) {
-  if (!isUser) {
-    var options = {username: siteConfig.userName};
-    if (siteConfig.homeDir) options.d = siteConfig.homeDir;
-    return linuxUser.addUser(options)
-      .then(result=>linuxUser.getUserInfo(siteConfig.userName));
+  function parseConfig(config) {
+    config.script = __dirname + '/server.js';
+    let template = _.template(JSON.stringify(config));
+    return JSON.parse(template(config));
   }
-  return true;
-}
 
-function addUser(siteConfig) {
-  if (siteConfig.userName) {
-    return linuxUser.isUser(siteConfig.userName).then(
-      isUser=>createUserIfNotCreated(isUser, siteConfig)
-    ).then(
-      isUser=>linuxUser.getUserInfo(siteConfig.userName)
-    ).then(user=>{
-      return chown(user.homedir, user.uid, user.gid).then(
-        result=>user
-      );
-    }).then(user=>{
-      siteConfig.uid = user.uid;
-      siteConfig.gid = user.gid;
-      return siteConfig;
-    });
-  } else {
-    return siteConfig;
+  function loadConfig(name) {
+    return dbBolt.loadMongo(config.db)
+      .then(db=>db.collection('configs').findOne({name}))
+      .then(parseConfig)
+      .then(siteConfig=>{
+        siteConfig.development = (siteConfig.hasOwnProperty('development') ? siteConfig.development : argv.development);
+        if (!linuxUser) siteConfig.development = true;
+        return siteConfig;
+      })
   }
-}
 
+  function getPm2Instances(name) {
+    return pm2.listAsync()
+      .filter(apps=>(apps.name === name));
+  }
 
-if (_.indexOf(argv._, 'start') !== -1) {
-  if (argv.hasOwnProperty('name')) {
-    loadConfig(argv.name).then(
-      siteConfig=>((!siteConfig.development) ? addUser(siteConfig) : siteConfig)
-    ).then(
-      siteConfig=>((!siteConfig.development) ? launchPm2(siteConfig) : launchApp(siteConfig)),
-      err=>console.log(err)
-    ).then(app=>{
-      if (app && app.pm2_env) {
-        console.log(app.pm2_env.name, 'launched with id:', app.pm2_env.pm_id);
-        process.exit();
-      }
+  function removeOldInstances(pm2Config) {
+    return getPm2Instances(pm2Config.name).then(apps=>(
+      apps.length ?
+        Promise.all(apps.map(app=>pm2.deleteAsync(app.pm2_env.pm_id))) :
+        true
+    ));
+  }
+
+  function startInstance(pm2Config, boltConfig) {
+    return pm2.startAsync(pm2Config).then(app=>{
+      const id = app[0].pm2_env.pm_id;
+      pm2.sendDataToProcessId(id, {type:'config', data:boltConfig, id, topic:'config'});
+      return pm2.disconnectAsync().then(()=>app[0]);
     });
   }
-}
+
+  function launchPm2(siteConfig) {
+    let pm2Config = _.pick(siteConfig, processFileProperties);
+    let boltConfig = _.pick(siteConfig, boltConfigProperties);
+
+    return pm2.connectAsync()
+      .then(()=>removeOldInstances(pm2Config))
+      .then(()=>startInstance(pm2Config, boltConfig));
+  }
+
+  function launchApp(siteConfig) {
+    let boltConfig = _.pick(siteConfig, boltConfigProperties);
+    launcher(boltConfig);
+  }
+
+  function createUserIfNotCreated(isUser, siteConfig) {
+    if (!isUser) {
+      var options = {username: siteConfig.userName};
+      if (siteConfig.homeDir) options.d = siteConfig.homeDir;
+      return linuxUser.addUser(options)
+        .then(result=>linuxUser.getUserInfo(siteConfig.userName));
+    }
+    return true;
+  }
+
+  function addUser(siteConfig) {
+    if (siteConfig.userName) {
+      return linuxUser.isUser(siteConfig.userName).then(
+        isUser=>createUserIfNotCreated(isUser, siteConfig)
+      ).then(
+        isUser=>linuxUser.getUserInfo(siteConfig.userName)
+      ).then(user=>{
+        return chown(user.homedir, user.uid, user.gid).then(
+          result=>user
+        );
+      }).then(user=>{
+        siteConfig.uid = user.uid;
+        siteConfig.gid = user.gid;
+        return siteConfig;
+      });
+    } else {
+      return siteConfig;
+    }
+  }
+
+
+  if (_.indexOf(argv._, 'start') !== -1) {
+    if (argv.hasOwnProperty('name')) {
+      loadConfig(argv.name).then(
+        siteConfig=>((!siteConfig.development) ? addUser(siteConfig) : siteConfig)
+      ).then(
+        siteConfig=>((!siteConfig.development) ? launchPm2(siteConfig) : launchApp(siteConfig)),
+        err=>console.log(err)
+      ).then(app=>{
+        if (app && app.pm2_env) {
+          console.log(app.pm2_env.name, 'launched with id:', app.pm2_env.pm_id);
+          process.exit();
+        }
+      });
+    }
+  }
+
+});
--- bolt/database.js
+++ bolt/database.js
@@ -1,214 +1,36 @@
 'use strict';
 
 const Promise = require('bluebird');
-const rrulestr = require('rrule').rrulestr;
-const dateParser = require('ical-date-parser');
 
+module.exports = require('require-extra').importDirectory('./database/interfaces/', {
+  merge: true
+}).then(interfaces=>{
 
-const loaders = {
-	mongodb: require('./database/mongo'),
-	mysql: require('./database/mysql')
-};
+  function _loadDatabases(app, config=app.config.databases) {
+    app.dbs = app.dbs || {};
+    let databases = Object.keys(config);
 
+    return Promise.all(databases.map(dbName => {
+      let options = config[dbName];
+      let loader = interfaces[options.type];
 
-function _loadDatabases(app, config=app.config.databases) {
-	app.dbs = app.dbs || {};
-	let databases = Object.keys(config);
-
-	return Promise.all(databases.map(dbName => {
-		let options = config[dbName];
-		let loader = loaders[options.type];
-
-		return loader(options).then(database => {
-			app.dbs[dbName] = database;
-			if (options.default) {
-				app.db = database;
-			}
-		});
-	})).then(() => app);
-}
-
-function _getAccessLevelLookup(acl, accessLevel) {
-  if (accessLevel === 'read') {
-    return (acl.security.administrators || []).concat(acl.security.readers || []).concat(acl.security.editors || [])
-  } else if (accessLevel === 'edit') {
-    return (acl.security.administrators || []).concat(acl.security.editors || [])
-  } else if (accessLevel === 'admin') {
-    return bolt.clone(acl.security.administrators || []);
-  }
-
-  return [];
-}
-
-function _getAccessGroups(session) {
-  let ids = (session.groups || []).map(group=>group._id).filter(id=>id);
-  if (session && session.passport && session.passport.user) ids.unshift(session.passport.user);
-  return ids;
-}
-
-function _idIsInGroup(groupIds, authorisedIds) {
-  let found;
-  groupIds.every(groupId=>{
-    authorisedIds.every(aid=>{
-      if (groupId.toString() === aid.toString()) found = aid;
-      return !found;
-    });
-    return !found;
-  });
-
-  return (found !== undefined);
-}
-
-function _isAuthorised(doc, session, accessLevel) {
-  if (!doc) return false;
-  if (!doc._acl) return false;
-  if (!doc._acl.security) return false;
-
-  let authorisedIds = _getAccessLevelLookup(doc._acl, accessLevel.toLowerCase().trim());
-  if (authorisedIds.length) {
-    let groupIds = _getAccessGroups(session);
-    return _idIsInGroup(groupIds, authorisedIds);
-  }
-
-  return false;
-}
-
-function _isAuthorisedVisibility(doc) {
-  if (doc && doc._acl && doc._acl.visibility && acl.visibility) {
-    let ruleString = '';
-    let acl = doc._acl;
-
-    Object.keys(acl.visibility).forEach(key=>{
-      if ((key === 'DTSTART') || (key === 'DTEND') || (key === 'fields')) return;
-      if (bolt.isString(acl.visibility[key])) {
-        ruleString += key + ':' + acl.visibility[key] + '\n';
-      } else if (bolt.isArray(acl.visibility[key])) {
-        ruleString += key + ':' + acl.visibility[key].map(item=>{
-          if (bolt.isString(item)) {
-            return item;
-          } else {
-            return Object.keys(item).map(itemId=>itemId + '=' + item[itemId]).join(';');
-          }
-        }).join(',') + '\n';
-      } else {
-        ruleString += key + ':' + Object.keys(acl.visibility[key]).map(itemId=>
-          itemId + '=' + acl.visibility[key][itemId]
-        ).join(';') + '\n';
-      }
-    });
-
-    let duration = dateParser(acl.visibility['DTEND']) - dateParser(acl.visibility['DTSTART']);
-    let rrule = rrulestr(ruleString);
-    let now = Date.now();
-    let ranges = rrule
-      .all()
-      .map(date=>Date.parse(date))
-      .filter(date=>(new Date(date+duration) > now))
-      .filter(date=>(date <= now));
-
-    return (ranges.length > 0);
-  }
-
-  return true;
-}
-
-function _parseOptions(options) {
-  options.accessLevel = options.accessLevel || 'read';
-  options.collection = options.collection || 'pages';
-  options.app = ((options.req && !options.app) ? options.req.app : options.app);
-  options.db = ((bolt.isString(options.db) && options.app) ? options.app.dbs[options.db] : options.db);
-  options.db = ((!options.db && options.app) ? options.app.db : options.db);
-  options.session = options.session || (options.req ? options.req.session : {});
-  if (options.id) options.id = mongoId(options.id);
-  if (Array.isArray(options.projection)) {
-    let temp = {};
-    options.projection.forEach(key=>{temp[key] = true;});
-    options.projection = temp;
-  }
-  if (!options.hasOwnProperty('filterByVisibility')) options.filterByVisibility = true;
-  if (!options.hasOwnProperty('filterByAccessLevel')) options.filterByAccessLevel = true;
-  if (!options.hasOwnProperty('filterUnauthorisedFields')) options.filterUnauthorisedFields = true;
-
-  return options;
-}
-
-function _prioritySorter(a, b) {
-  return bolt.prioritySorter({priority: a._priority}, {priority: b._priority});
-}
-
-function _removeUnauthorisedFields(doc, session, accessLevel) {
-  if (doc && doc._acl && doc._acl.fields) {
-    Object.keys(doc._acl.fields || {})
-      .map(field=> {
-        let auth = _isAuthorised(doc._acl.fields[field], session, accessLevel);
-        if (!auth) return field;
-        return (!_isAuthorisedVisibility(doc._acl.fields[field]) ? field : undefined);
-      })
-      .filter(field=>field)
-      .forEach(field=>{
-        if (doc.hasOwnProperty(field)) delete doc[field];
+      return loader(options).then(database => {
+        app.dbs[dbName] = database;
+        if (options.default) {
+          app.db = database;
+        }
       });
-
-    delete doc._acl;
+    })).then(() => app);
   }
 
-  return doc;
-}
-
-function _getDoc(options) {
-  if (options.id) {
-    let query = Object.assign({_id: options.id}, options.query || {});
-    return options.db.collection(options.collection).findOne(query, options.projection);
-  } else {
-    return options.db.collection(options.collection).find(options.query, options.projection).toArray();
+  function loadDatabases(app) {
+    return bolt.fire(()=>_loadDatabases(app), 'loadDatabases', app).then(() => app);
   }
-}
 
-function getDocs(options) {
-  let _options = _parseOptions(options);
 
-  return _getDoc(_options).then(docs=>bolt.makeArray(docs))
-}
-
-function getDoc(options) {
-  let _options = _parseOptions(options);
-
-  return _getDoc(_options)
-    .then(docs=>bolt.makeArray(docs))
-    .then(docs=>
-      (options.filterByAccessLevel ? docs.filter(doc=>_isAuthorised(doc, _options.session, _options.accessLevel)) : docs)
-    )
-    .then(docs=>
-      (options.filterByVisibility ? docs.filter(doc=>_isAuthorisedVisibility(doc)) : docs)
-    )
-    .then(docs=>docs.sort(_prioritySorter))
-    .then(docs=>
-      (options.filterUnauthorisedFields ? _removeUnauthorisedFields(docs[0], _options.session, _options.accessLevel) : docs[0])
-    )
-}
-
-function removeUnauthorisedFields(options) {
-  let _options = _parseOptions(options);
-  return _removeUnauthorisedFields(_options.doc, _options.session, _options.accessLevel);
-}
-
-function isAuthorised(options) {
-  let _options = _parseOptions(options);
-  return _getDoc(options).then(
-    doc=>_isAuthorised(doc, _options.session, _options.accessLevel)
-  );
-}
-
-function loadDatabases(app) {
-  return bolt.fire(()=>_loadDatabases(app), 'loadDatabases', app).then(() => app);
-}
-
-module.exports = {
-	loadDatabases,
-  mongoId:loaders.mongodb.mongoId,
-  getDoc,
-  getDocs,
-  loadMongo:loaders.mongodb,
-  isAuthorised,
-  removeUnauthorisedFields
-};
+  return Object.assign({
+    loadDatabases,
+    mongoId:interfaces.mongodb.mongoId,
+    loadMongo:interfaces.mongodb
+  }, require('./database/collectionLogic'));
+});
