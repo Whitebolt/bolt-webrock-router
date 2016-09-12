c9a3a09a6963579ce55cc4abe2d409300725bb3f
Stephen <stephen@whitebolt.net>
2016-09-12 10:23:54 +0100

    Add new row on login
    You do not want to change the login line but always create a new one.

--- middleware/auth.js
+++ middleware/auth.js
@@ -85,22 +85,9 @@ function init(app) {
 			return db.query({
 				type: 'select',
 				table: 'user_log',
-				where: {wr_bolt_hash: req.sessionID}
+				where: {wr_bolt_hash: req.sessionID, logged_out:0}
 			}).spread(rows=>{
-				if (rows.length) {
-					if (rows[0].logged_out && (rows[0].logged_out === 1)) {
-						return db.query({
-							type: 'update',
-							table: 'user_log',
-							updates: {
-								logged_out: 0
-							},
-							where: {wr_bolt_hash: req.sessionID}
-						});
-					} else {
-						return Promise.resolve();
-					}
-				} else {
+				if (!rows.length) {
 					return db.query({
 						type: 'insert',
 						table: 'user_log',
@@ -153,7 +140,6 @@ function init(app) {
 	app.all('/*', (req, res, next)=>{
 		let logout = fieldFromGetOrPost(req, 'wr_user_logout');
 		if (parseInt(logout) === 1) {
-			//delete req.body.wr_user_logout;
 			if (req && req.sessionID && req.session && req.session.passport && req.session.passport.user) {
 				db.query({
 					type: 'update',
