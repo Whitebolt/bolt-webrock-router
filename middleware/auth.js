a0aa94e0cf9ecf9dc624429696672502069dd563
Stephen <stephen@whitebolt.net>
2017-02-20 13:27:19 +0000

    Major refactor
    Attempt here to group related code together in one database Api.

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,75 +16,139 @@ function getWebRockDb(app) {
 	}
 }
 
+function cloneObject(obj) {
+	let cloned = {};
+	Object.keys(obj).forEach(fieldName=>{
+		cloned[fieldName] = obj[fieldName];
+	});
+	return cloned;
+}
+
 function init(app) {
-	let db = getWebRockDb(app);
+	const db = getWebRockDb(app);
+
+	const dbApi = {
+		getUserByLogin: function (username, password) {
+			return db.query({
+				type: 'select',
+				table: 'user',
+				where: {
+					$and: {
+						$or: {name:username, email:username},
+						password: md5(password).toLowerCase(),
+						isactive: 1
+					}
+				}
+			}).spread(rows=>(rows.length?rows[0]:Promise.reject()));
+		},
+
+		getUserById: function(id) {
+			if (!id) return Promise.resolve({});
+			return db.query({
+				type: 'select',
+				table: 'user',
+				where: {id}
+			}).spread(rows=>{
+				if (!rows.length) return Promise.reject('User not found');
+				return rows[0];
+			}).then(row=>{
+				let user = {};
+				Object.keys(row).forEach(fieldName=>{
+					user[fieldName] = row[fieldName];
+				});
+				return user;
+			});
+		},
+
+		getUserByLoginHash: function(userId, hash) {
+			return db.query({
+				type: 'select',
+				table: 'user',
+				where: {id:userId, h:hash}
+			}).spread(rows=>(rows.length?rows[0]:Promise.reject('User not found')));
+		},
+
+		getLogRowBySessionId: function(sessionId) {
+			return db.query({
+				type: 'select',
+				table: 'user_log',
+				where: {wr_bolt_hash: sessionId, logged_out: 0}
+			}).then(rows=>(rows.length?rows[0]:Promise.reject()));
+		},
+
+		updateUserPassword: function(password, where) {
+			return db.query({
+				type: 'update',
+				table: 'user',
+				updates: {isactive: 1, password: md5(password)},
+				where
+			});
+		},
+
+		logFailedLogin: function(ip, username) {
+			return db.query({
+				type: 'insert',
+				table: 'user_log',
+				values: {
+					wr_bolt_hash: req.sessionID,
+					user_id: 0,
+					ip,
+					failed: 1,
+					logged_out: 1,
+					failed_name: username,
+					sentdate: bolt.dateFormat(new Date(), 'yyyy-mm-dd hh:MM;ss')
+				}
+			});
+		},
+
+		logNewSession: function(userId, ip, sessionId) {
+			return db.query({
+				type: 'insert',
+				table: 'user_log',
+				values: {
+					wr_bolt_hash: sessionId,
+					user_id: userId,
+					ip,
+					failed: 0,
+					logged_out: 0,
+					sentdate: bolt.dateFormat(new Date(), 'yyyy-mm-dd hh:MM;ss')
+				}
+			});
+		},
+
+		logExpiredSession: function(sessionId) {
+			return db.query({
+				type: 'update',
+				table: 'user_log',
+				updates: {
+					logged_out: 1
+				},
+				where: {wr_bolt_hash: sessionId}
+			})
+		}
+
+	};
+
+
+	function handleFailedLogin(username) {
+		return dbApi.logFailedLogin(getIp(req), username)
+			.then(()=>Promise.reject('User not found'));
+	}
 
 	function login(req, username, password, done) {
 		if (!db) return done(null, false);
-		db.query({
-			type: 'select',
-			table: 'user',
-			where: {
-				$and: {
-					$or: {name:username, email:username},
-					password: md5(password).toLowerCase(),
-					isactive: 1
-				}
-			}
-		}).spread(rows=>{
-			let ip = getIp(req);
-			if (!rows.length) {
-				return db.query({
-					type: 'insert',
-					table: 'user_log',
-					values: {
-						wr_bolt_hash: req.sessionID,
-						user_id: 0,
-						ip,
-						failed: 1,
-						logged_out: 1,
-						failed_name: username,
-						sentdate: bolt.dateFormat(new Date(), 'yyyy-mm-dd hh:MM;ss')
-					}
-				}).then(()=>{
-					bolt.fire("webRockFailedLogin", username, ip);
-					return Promise.reject('User not found');
-				})
-			}
-			bolt.fire("webRockLogin", username, ip);
-			return rows[0];
-		}).then(row=>{
-			let user = {};
-			Object.keys(row).forEach(fieldName=>{
-				user[fieldName] = row[fieldName];
-			});
-			return done(null, user);
-		}).catch(err=>{
-			return done(null, err);
-		});
-	}
-
-	function getUserById(id) {
-		if (!id) return Promise.resolve({});
-		return db.query({
-			type: 'select',
-			table: 'user',
-			where: {id}
-		}).spread(rows=>{
-			if (!rows.length) return Promise.reject('User not found');
-			return rows[0];
-		}).then(row=>{
-			let user = {};
-			Object.keys(row).forEach(fieldName=>{
-				user[fieldName] = row[fieldName];
-			});
-			return user;
+		dbApi.getUserByLogin(username, password).then(user=>{
+			bolt.fire("webRockLogin", username, getIp(req));
+			return done(null, cloneObject(user));
+		}, ()=>{
+			bolt.fire("webRockFailedLogin", username, ip);
+			return handleFailedLogin(req, username);
 		});
 	}
 
 	function populateUserSessionData(session){
 		let id = session.passport.user.toString();
-		return getUserById(id).then(user=>{
+		return dbApi.getUserById(id).then(user=>{
 			session.user = user;
 			return user;
 		}, err=>{
@@ -101,51 +165,18 @@ function init(app) {
 
 	function addToSessionTable(req) {
 		if (req && req.sessionID && req.session && req.session.passport && req.session.passport.user) {
-			return db.query({
-				type: 'select',
-				table: 'user_log',
-				where: {wr_bolt_hash: req.sessionID, logged_out:0}
-			}).spread(rows=>{
-				if (!rows.length) {
-					return db.query({
-						type: 'insert',
-						table: 'user_log',
-						values: {
-							wr_bolt_hash: req.sessionID,
-							user_id: parseInt(req.session.passport.user, 10),
-							ip: getIp(req),
-							failed: 0,
-							logged_out: 0,
-							sentdate: bolt.dateFormat(new Date(), 'yyyy-mm-dd hh:MM;ss')
-						}
-					});
-				}
+			return dbApi.getLogRowBySessionId(req.sessionID).then(logRow=>{
+				return dbApi.logNewSession(parseInt(req.session.passport.user, 10), getIp(req), req.sessionID);
 			});
 		}
 		return Promise.resolve();
 	}
 
-	app.use(passport.initialize());
-	app.use(passport.session());
-
-	passport.use(new Strategy({
-		usernameField: 'wr_username',
-		passwordField: 'wr_password',
-		session: true,
-		passReqToCallback: true
-	}, login));
-
-	passport.serializeUser((data, callback)=>{
-		if (data.id) return callback(null, data.id.toString());
-		return callback(null, 0);
-	});
-
-	passport.deserializeUser((id, callback)=>getUserById(id).nodeify(callback));
-
 	function fieldFromGetOrPost(req, fieldName) {
 		let query = bolt.merge({}, req.query, req.body || {});
 		return query[fieldName];
 	}
+
 
 	function webRockAuth(req, res, next) {
 		if (req.body && req.body.wr_username && req.body.wr_password) {
@@ -167,14 +198,7 @@ function init(app) {
 		let logout = fieldFromGetOrPost(req, 'wr_user_logout');
 		if (parseInt(logout) === 1) {
 			if (req && req.sessionID && req.session && req.session.passport && req.session.passport.user) {
-				return db.query({
-					type: 'update',
-					table: 'user_log',
-					updates: {
-						logged_out: 1
-					},
-					where: {wr_bolt_hash: req.sessionID}
-				}).then(()=>{
+				return dbApi.logExpiredSession(req.sessionID).then(()=>{
 					req.logout();
 					let username = ((req && req.session && req.session.user) ? req.session.user.name : 'Unknown');
 					bolt.fire("webRockLogout", username, getIp(req));
@@ -189,36 +213,55 @@ function init(app) {
 		return next();
 	}
 
-	function webRockAuthViaEmail(req, res, next) {
+	function webRockAuthViaEmail1(req, res, next) {
 		let pathParts = bolt.splitAndTrim(req.path, '/');
 		let id = parseInt(pathParts[1], 10);
 		let h = bolt.queryStringToObject(pathParts[2]).h;
 		let email = '';
 
-		return db.query({
-			type: 'select',
-			table: 'user',
-			where: {id, h}
-		}).spread(rows=>{
-			if (!rows.length) return Promise.reject('User not found');
-
-			let wr_username = rows[0]['email'];
+		return dbApi.getUserByLoginHash(id, h).then(user=>{
+			let wr_username = user.email;
 			let wr_password = bolt.randomString();
 
 			req.body = {wr_username, wr_password};
 			req.method = 'post';
 
-			return db.query({
-				type: 'update',
-				table: 'user',
-				updates: {isactive: 1, password: md5(wr_password)},
-				where: {id, h}
-			});
+			return dbApi.updateUserPassword(wr_password, {id, h});
 		}).then(()=>next());
 	}
 
+	function webRockAuthViaEmail2(req, res, next) {
+		let pathParts = bolt.splitAndTrim(req.path, '/');
+		let id = parseInt(pathParts[1], 10);
+		let h = bolt.queryStringToObject(pathParts[2]).h;
+
+		return dbApi.updateUserPassword(null, {id, h}).then(()=>{
+			res.redirect('/members-home/password');
+			return next();
+		});
+	}
+
+
+	app.use(passport.initialize());
+	app.use(passport.session());
+
+	passport.use(new Strategy({
+		usernameField: 'wr_username',
+		passwordField: 'wr_password',
+		session: true,
+		passReqToCallback: true
+	}, login));
+
+	passport.serializeUser((data, callback)=>{
+		if (data.id) return callback(null, data.id.toString());
+		return callback(null, 0);
+	});
+
+	passport.deserializeUser((id, callback)=>dbApi.getUserById(id).nodeify(callback));
+
+
 	app.post('/*', webRockAuth, webRockAuthAddSession);
-	app.get(/\/login\/\d+\/h\=[a-fA-F0-9]{32,32}/, webRockAuthViaEmail, webRockAuth, webRockAuthAddSession);
+	app.get(/\/login\/\d+\/h\=[a-fA-F0-9]{32,32}/, webRockAuthViaEmail1, webRockAuth, webRockAuthAddSession, webRockAuthViaEmail2);
 	app.all('/*', webRockLogout)
 
 	app.all('/*', (req, res, next)=>{
@@ -233,6 +276,8 @@ function init(app) {
 		req.session.save();
 		return next();
 	});
+
+
 }
 
 init.priority = 3;
