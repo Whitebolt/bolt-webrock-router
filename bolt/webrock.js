61727340b93387788f2b42524a9ff0a26fe545e9
Stephen <stephen@whitebolt.net>
2017-03-09 08:56:40 +0000

    Extract dbApi to bolt object
    Move the WebRock api into a global bolt object.

---
+++ bolt/webrock.js
@@ -0,0 +1,218 @@
+'use strict';
+
+const Promise = module.parent.require('bluebird');
+const WebRockError = require('../lib/errors').WebRockError;
+const WebRockDatabaseError = require('../lib/errors').WebRockDatabaseError;
+
+let _db;
+
+/**
+ * Clone an object without copying the prototype.
+ *
+ * @note	We are using this to clone returned database objects.  Just cloning
+ * 			the fields and none of the class logic returned by the
+ * 			database interface.
+ *
+ * @private
+ * @param {Object} obj		Object to clone.
+ * @returns {Object}		Cloned object.
+ */
+function _cloneObject(obj) {
+	let cloned = {};
+	Object.keys(obj).forEach(fieldName=>{
+		cloned[fieldName] = obj[fieldName];
+	});
+	return cloned;
+}
+
+/**
+ * Get a user from the user table using the given where clause.
+ *
+ * @private
+ * @param {Object} where			Json style where clause.
+ * @param {Object} db=_db			Database to use.
+ * @returns {Promise<Object>}		Promise resolving when done or rejecting
+ * 									if operation fails.
+ */
+function _getUser(where, db) {
+	return db.query({type: 'select', table: 'user', where});
+}
+
+/**
+ * Get a user db row form user table by using the username and password.
+ *
+ * @public
+ * @param {string} username				Username.
+ * @param {string} clearTextPassword	Password in clear text.
+ * @param {Object} db=_db				Database to use.
+ * @returns {Promise<Object>}			Promise resolving database row
+ * 										or error 'User not found'.
+ */
+function getUserByLogin(username, clearTextPassword, db=_db) {
+	let password = md5(clearTextPassword).toLowerCase();
+	if (!bolt.isString(username)) return Promise.reject(WebRockDatabaseError('Invalid username given to database query'));
+
+	return _getUser({
+		$and: {
+			$or: {name:username, email:username},
+			password,
+			isactive: 1
+		}
+	}, db).spread(
+		rows=>(rows.length ? _cloneObject(rows[0]) : Promise.reject(new WebRockDatabaseError(`User not found in database for user: ${username} and password-hash: ${password}`)))
+	);
+}
+
+/**
+ * Get a user db row form user table by using the user id.
+ *
+ * @public
+ * @param {integer} id			User id.
+ * @param {Object} db=_db		Database to use.
+ *
+ * @returns {Promise<Object>}	Promise resolving database row or error
+ * 								'User not found'.
+ */
+function getUserById(id, db=_db) {
+	if (!bolt.isNumeric(id) && !bolt.isInteger(parseInt(id, 10))) return Promise.reject(new WebRockDatabaseError(`Invalid user id (${id}) given to database query`));
+
+	if (!id) return Promise.resolve({});
+	return _getUser({id}, db).spread(
+		rows=>(rows.length ? _cloneObject(rows[0]) : Promise.reject(new WebRockDatabaseError(`User not found in database for user id: ${id}`)))
+	);
+}
+
+/**
+ * Get a user db row form user table by using the user id and
+ * login hash (as would be sent in an email).
+ *
+ * @public
+ * @param {integer} id			User id.
+ * @param {string} loginHash	User session hash.
+ * @param {Object} db=_db		Database to use.
+ * @returns {Promise<Object>}	Promise resolving database row or error
+ * 								'User not found'.
+ */
+function getUserByLoginHash(id, loginHash, db=_db) {
+	if (!bolt.isNumeric(id) && !bolt.isInteger(parseInt(id, 10))) return Promise.reject(new WebRockDatabaseError(`Invalid user id (${id}) given to database query`));
+	if (!bolt.isString(loginHash)) return Promise.reject(new WebRockDatabaseError(`Invalid session hash given to database query: ${loginHash}`));
+
+	return _getUser({id, h:loginHash}, db).spread(
+		rows=>(rows.length ? _cloneObject(rows[0]) : Promise.reject(new WebRockDatabaseError(`User not found in database for user id: ${id} and session hash: ${loginHash}`)))
+	);
+}
+
+/**
+ * Get user log row from database from given session id.
+ *
+ * @public
+ * @param {string} sessionId		Session hash.
+ * @param {Object} db=_db			Database to use.
+ * @returns {Promise<Object>}		Promise resolving database row or error
+ * 									'Log entry not found'.
+ */
+function getLogRowBySessionId(sessionId, db=_db) {
+	return db.query({
+		type: 'select',
+		table: 'user_log',
+		where: {wr_bolt_hash: sessionId, logged_out: 0}
+	}).spread(rows=>(rows.length ? rows[0] : Promise.reject(new WebRockDatabaseError(`Log entry not found for session id: ${sessionId}`))));
+}
+
+/**
+ * Get user log row from database from given session id.
+ *
+ * @public
+ * @param {string} sessionId		Session hash.
+ * @param {Object} db=_db			Database to use.
+ * @returns {Promise<Object>}		Promise resolving when done or rejecting if
+ * 									there is an error.
+ */
+function updateUserPassword(password, where, db=_db) {
+	return db.query({
+		type: 'update',
+		table: 'user',
+		updates: {isactive: 1, password: ((password===null)?null:md5(password))},
+		where
+	});
+}
+
+/**
+ * Insert a failed login entry into the the log.
+ *
+ * @public
+ * @param {string} ip			IP of failed client.
+ * @param {string} username		Username of failed client.
+ * @param {Object} db=_db		Database to use.
+ * @returns {Promise<Object>}	Promise resolving when done or rejecting
+ * 								if operation fails.
+ */
+function logFailedLogin(ip, username, db=_db) {
+	return db.query({
+		type: 'insert',
+		table: 'user_log',
+		values: {
+			wr_bolt_hash: req.sessionID,
+			user_id: 0,
+			ip,
+			failed: 1,
+			logged_out: 1,
+			failed_name: username,
+			sentdate: bolt.dateFormat(new Date(), 'yyyy-mm-dd hh:MM;ss')
+		}
+	});
+}
+
+/**
+ * Log a new session in the session table.
+ *
+ * @public
+ * @param {integer} userId			User id of user.
+ * @param {string} ip				IP of user.
+ * @param {string} sessionId		Session id of user.
+ * @param {Object} db=_db			Database to use.
+ * @returns {Promise<Object>}		Promise resolving when done or rejecting
+ * 									if operation fails.
+ */
+function logNewSession(userId, ip, sessionId, db=_db) {
+	return db.query({
+		type: 'insert',
+		table: 'user_log',
+		values: {
+			wr_bolt_hash: sessionId,
+			user_id: userId,
+			ip,
+			failed: 0,
+			logged_out: 0,
+			sentdate: bolt.dateFormat(new Date(), 'yyyy-mm-dd hh:MM;ss')
+		}
+	});
+}
+
+/**
+ * Set a session to expired.
+ *
+ * @public
+ * @param {string} 				Session id of user.
+ * @param {Object} db=_db		Database to use.
+ * @returns {Promise<Object>}	Promise resolving when done or rejecting
+ * 								if operation fails.
+ */
+function logExpiredSession(sessionId, db=_db) {
+	return db.query({
+		type: 'update',
+		table: 'user_log',
+		updates: {
+			logged_out: 1
+		},
+		where: {wr_bolt_hash: sessionId}
+	})
+}
+
+function setDb(db) {
+	_db = db;
+}
+
+module.exports = {webrock: {
+	getUserByLogin, getUserById, getUserByLoginHash, getLogRowBySessionId, updateUserPassword, logFailedLogin, logNewSession, logExpiredSession, setDb
+}};
--- middleware/auth.js
+++ middleware/auth.js
@@ -6,8 +6,8 @@ const Promise = module.parent.require('bluebird');
 const md5 = module.parent.require('md5');
 const session = module.parent.require('express-session');
 
-class WebRockError extends Error {}
-class WebRockDatabaseError extends WebRockError {}
+const WebRockError = require('../lib/errors').WebRockError;
+const WebRockDatabaseError = require('../lib/errors').WebRockDatabaseError;
 
 function getIp(req) {
 	return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -24,207 +24,10 @@ function getWebRockDb(app) {
 	if (app && app.dbs && app.dbs.webRock) return app.dbs.webRock;
 }
 
-/**
- * Clone an object without copying the prototype.
- *
- * @note	We are using this to clone returned database objects.  Just cloning
- * 			the fields and none of the class logic returned by the
- * 			database interface.
- *
- * @private
- * @param {Object} obj		Object to clone.
- * @returns {Object}		Cloned object.
- */
-function cloneObject(obj) {
-	let cloned = {};
-	Object.keys(obj).forEach(fieldName=>{
-		cloned[fieldName] = obj[fieldName];
-	});
-	return cloned;
-}
-
 function init(app) {
 	const db = getWebRockDb(app);
 
-	/**
-	 * Get a user from the user table using the given where clause.
-	 *
-	 * @private
-	 * @param {Object} where		Json style where clause.
-	 * @returns {Promise<Object>}	Promise resolving when done or rejecting
-	 * 								if operation fails.
-	 */
-	function getUser(where) {
-		return db.query({type: 'select', table: 'user', where});
-	}
-
-	const dbApi = {
-
-		/**
-		 * Get a user db row form user table by using the username and password.
-		 *
-		 * @public
-		 * @param {string} username				Username.
-		 * @param {string} clearTextPassword	Password in clear text.
-		 * @returns {Promise<Object>}			Promise resolving database row
-		 * 										or error 'User not found'.
-		 */
-		getUserByLogin: function (username, clearTextPassword) {
-			let password = md5(clearTextPassword).toLowerCase();
-			if (!bolt.isString(username)) return Promise.reject(WebRockDatabaseError('Invalid username given to database query'));
-
-			return getUser({
-				$and: {
-					$or: {name:username, email:username},
-					password,
-					isactive: 1
-				}
-			}).spread(
-				rows=>(rows.length ? cloneObject(rows[0]) : Promise.reject(new WebRockDatabaseError(`User not found in database for user: ${username} and password-hash: ${password}`)))
-			);
-		},
-
-		/**
-		 * Get a user db row form user table by using the user id.
-		 *
-		 * @public
-		 * @param {integer} id			User id.
-		 *
-		 * @returns {Promise<Object>}	Promise resolving database row or error
-		 * 								'User not found'.
-		 */
-		getUserById: function(id) {
-			if (!bolt.isNumeric(id) && !bolt.isInteger(parseInt(id, 10))) return Promise.reject(new WebRockDatabaseError(`Invalid user id (${id}) given to database query`));
-
-			if (!id) return Promise.resolve({});
-			return getUser({id}).spread(
-				rows=>(rows.length ? cloneObject(rows[0]) : Promise.reject(new WebRockDatabaseError(`User not found in database for user id: ${id}`)))
-			);
-		},
-
-		/**
-		 * Get a user db row form user table by using the user id and
-		 * login hash (as would be sent in an email).
-		 *
-		 * @public
-		 * @param {integer} id			User id.
-		 * @param {string} loginHash	User session hash
-		 * @returns {Promise<Object>}	Promise resolving database row or error
-		 * 								'User not found'.
-		 */
-		getUserByLoginHash: function(id, loginHash) {
-			if (!bolt.isNumeric(id) && !bolt.isInteger(parseInt(id, 10))) return Promise.reject(new WebRockDatabaseError(`Invalid user id (${id}) given to database query`));
-			if (!bolt.isString(loginHash)) return Promise.reject(new WebRockDatabaseError(`Invalid session hash given to database query: ${loginHash}`));
-
-			return getUser({id, h:loginHash}).spread(
-				rows=>(rows.length ? cloneObject(rows[0]) : Promise.reject(new WebRockDatabaseError(`User not found in database for user id: ${id} and session hash: ${loginHash}`)))
-			);
-		},
-
-		/**
-		 * Get user log row from database from given session id.
-		 *
-		 * @public
-		 * @param {string} sessionId	Session hash
-		 * @returns {Promise<Object>}	Promise resolving database row or error
-		 * 								'Log entry not found'.
-		 */
-		getLogRowBySessionId: function(sessionId) {
-			return db.query({
-				type: 'select',
-				table: 'user_log',
-				where: {wr_bolt_hash: sessionId, logged_out: 0}
-			}).spread(rows=>(rows.length ? rows[0] : Promise.reject(new WebRockDatabaseError(`Log entry not found for session id: ${sessionId}`))));
-		},
-
-		/**
-		 * Get user log row from database from given session id.
-		 *
-		 * @public
-		 * @param {string} sessionId	Session hash
-		 * @returns {Promise<Object>}	Promise resolving when done or
-		 * 								rejecting if there is an error.
-		 */
-		updateUserPassword: function(password, where) {
-			return db.query({
-				type: 'update',
-				table: 'user',
-				updates: {isactive: 1, password: ((password===null)?null:md5(password))},
-				where
-			});
-		},
-
-		/**
-		 * Insert a failed login entry into the the log.
-		 *
-		 * @public
-		 * @param {string} ip			IP of failed client.
-		 * @param {string} username		Username of failed client.
-		 * @returns {Promise<Object>}	Promise resolving when done or rejecting
-		 * 								if operation fails.
-		 */
-		logFailedLogin: function(ip, username) {
-			return db.query({
-				type: 'insert',
-				table: 'user_log',
-				values: {
-					wr_bolt_hash: req.sessionID,
-					user_id: 0,
-					ip,
-					failed: 1,
-					logged_out: 1,
-					failed_name: username,
-					sentdate: bolt.dateFormat(new Date(), 'yyyy-mm-dd hh:MM;ss')
-				}
-			});
-		},
-
-		/**
-		 * Log a new session in the session table.
-		 *
-		 * @public
-		 * @param {interger} userId		User id of user.
-		 * @param {string} ip			IP of user.
-		 * @param {string} sessionId	Session id of user.
-		 * @returns {Promise<Object>}	Promise resolving when done or rejecting
-		 * 								if operation fails.
-		 */
-		logNewSession: function(userId, ip, sessionId) {
-			return db.query({
-				type: 'insert',
-				table: 'user_log',
-				values: {
-					wr_bolt_hash: sessionId,
-					user_id: userId,
-					ip,
-					failed: 0,
-					logged_out: 0,
-					sentdate: bolt.dateFormat(new Date(), 'yyyy-mm-dd hh:MM;ss')
-				}
-			});
-		},
-
-		/**
-		 * Set a session to expired.
-		 *
-		 * @public
-		 * @param {string} 				Session id of user.
-		 * @returns {Promise<Object>}	Promise resolving when done or rejecting
-		 * 								if operation fails.
-		 */
-		logExpiredSession: function(sessionId) {
-			return db.query({
-				type: 'update',
-				table: 'user_log',
-				updates: {
-					logged_out: 1
-				},
-				where: {wr_bolt_hash: sessionId}
-			})
-		}
-
-	};
-
+	bolt.webrock.setDb(db);
 
 	/**
 	 * Handle a failed login
@@ -235,7 +38,7 @@ function init(app) {
 	 * @returns {Promise<Object>}	Throws the promise with 'User not found'.
 	 */
 	function handleFailedLogin(req, username) {
-		return dbApi.logFailedLogin(getIp(req), username)
+		return bolt.webrock.logFailedLogin(getIp(req), username)
 			.throw(new WebRockDatabaseError(`User not found in database for username: ${username}`));
 	}
 
@@ -252,7 +55,7 @@ function init(app) {
 	 */
 	function login(req, username, cleartextPassword, done) {
 		if (!db) return done(null, false);
-		dbApi.getUserByLogin(username, cleartextPassword).then(user=>{
+		bolt.webrock.getUserByLogin(username, cleartextPassword).then(user=>{
 			bolt.fire("webRockLogin", username, getIp(req));
 			return done(null, user);
 		}, ()=>{
@@ -273,7 +76,7 @@ function init(app) {
 	 */
 	function populateUserSessionData(session){
 		let id = session.passport.user.toString();
-		return dbApi.getUserById(id).then(user=>{
+		return bolt.webrock.getUserById(id).then(user=>{
 			session.user = user;
 			return user;
 		}, err=>{
@@ -307,8 +110,8 @@ function init(app) {
 	 */
 	function addToSessionTable(req) {
 		if (req && req.sessionID && req.session && req.session.passport && req.session.passport.user) {
-			return dbApi.getLogRowBySessionId(req.sessionID)
-				.catch(err=>dbApi.logNewSession(parseInt(req.session.passport.user, 10), getIp(req), req.sessionID));
+			return bolt.webrock.getLogRowBySessionId(req.sessionID)
+				.catch(err=>bolt.webrock.logNewSession(parseInt(req.session.passport.user, 10), getIp(req), req.sessionID));
 		}
 		return Promise.resolve();
 	}
@@ -360,8 +163,8 @@ function init(app) {
 			delete req.body.wr_username;
 			delete req.body.wr_password;
 
-			return dbApi.getLogRowBySessionId(req.sessionID).then(
-				()=>dbApi.logExpiredSession(req.sessionID),
+			return bolt.webrock.getLogRowBySessionId(req.sessionID).then(
+				()=>bolt.webrock.logExpiredSession(req.sessionID),
 				()=>true
 			).then(
 				()=>addToSessionTable(req)
@@ -391,7 +194,7 @@ function init(app) {
 		let username;
 		if (parseInt(logout) === 1) {
 			if (req && req.sessionID && req.session && req.session.passport && req.session.passport.user) {
-				return dbApi.logExpiredSession(req.sessionID).then(()=>{
+				return bolt.webrock.logExpiredSession(req.sessionID).then(()=>{
 					req.logout();
 					username = ((req && req.session && req.session.user) ? req.session.user.name : 'Unknown');
 					bolt.fire("webRockLogout", username, getIp(req));
@@ -425,14 +228,14 @@ function init(app) {
 		let h = bolt.queryStringToObject(pathParts[2]).h;
 		let email = '';
 
-		return dbApi.getUserByLoginHash(id, h).then(user=>{
+		return bolt.webrock.getUserByLoginHash(id, h).then(user=>{
 			let wr_username = user.email;
 			let wr_password = bolt.randomString();
 
 			req.body = {wr_username, wr_password};
 			req.method = 'post';
 
-			return dbApi.updateUserPassword(wr_password, {id, h});
+			return bolt.webrock.updateUserPassword(wr_password, {id, h});
 		}).then(()=>next(), err=>{
 			console.error('Failed on webRockAuthViaEmail1', err);
 			return new WebRockError(`Failed to log user in from email link from login hash: ${h}.`);
@@ -454,7 +257,7 @@ function init(app) {
 		let id = parseInt(pathParts[1], 10);
 		let h = bolt.queryStringToObject(pathParts[2]).h;
 
-		return dbApi.updateUserPassword(null, {id, h}).then(()=>{
+		return bolt.webrock.updateUserPassword(null, {id, h}).then(()=>{
 			res.redirect('/members-home/password');
 			return next();
 		}, err=>{
@@ -479,7 +282,7 @@ function init(app) {
 		return callback(null, 0);
 	});
 
-	passport.deserializeUser((id, callback)=>dbApi.getUserById(id).nodeify(callback));
+	passport.deserializeUser((id, callback)=>bolt.webrock.getUserById(id).nodeify(callback));
 
 
 	app.post('/*', webRockAuth, webRockAuthAddSession);
